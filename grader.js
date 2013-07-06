20501f49a77954494c32c24e41f162a8e46df1bb
Jonathan Waltz <jon@waltzhome.com>
2013-07-06 23:03:11 +0000

    Fixed executable problem with grader.js

--- grader.js
+++ grader.js
@@ -26,8 +26,6 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var restler = require('restler');
-var URL_DEFAULT = "index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -67,7 +65,6 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_file', 'URL to index.html', clone(assertFileExists), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
