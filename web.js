1f910f3b89bf5550d15bd79b6096b14d29d151b1
Jonathan Waltz <jon@waltzhome.com>
2013-08-14 22:56:37 +0000

    Changed title of index, small css

--- web.js
+++ web.js
@@ -27,7 +27,7 @@ app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
 //ROUTES
 app.get('/', function(request, response) {
     response.render('index', { 
-        title: "Find Paleo Food Nearby" 
+        title: "Welcome to PaleoGrinds" 
     });
 });
 
