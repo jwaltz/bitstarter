e6747951388ee06ce249fa8d38fe5069bb7a9d3a
Jonathan Waltz <jon@waltzhome.com>
2013-08-14 16:42:41 +0000

    Modified/Deleted dependencies, got Views working

--- web.js
+++ web.js
@@ -1,29 +1,42 @@
 var fs = require('fs');
 var express = require('express');
 
-var app = express.createServer();
+var app = express();
 
+
+//VIEWS: TEMPLATING WITH EJS
+//Allows naming of views with .html extension instead of .ejs
+app.engine('.html', require('ejs').__express);
+
+//Optional since express defaults to CWD/views
+app.set('views', __dirname + '/views');
+
+//Without this an extension is needed when calling res.render()
+//ex: res.render('users.html') vs. res.render('users')
+app.set('view engine', 'html')
+
+
+//MIDDLEWARE
 app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
 app.use(express.cookieParser());
 app.use(express.session({secret: 'ceiling cat'}));
 app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
 
+
+//ROUTES
 app.get('/', function(request, response) {
-    fs.readFile('index.html', function(err, data) {
-        if (err) throw err;
-        response.send(data);
-    });
+    response.render('index');
 });
 
-app.get('/login.html', function(request, response) {
-    fs.readFile('login.html', function(err, data) {
-        if (err) throw err;
-        response.send(data);
-    });
+app.get('/login', function(request, response) {
+    response.render('login');
 });
 
+
+//START SERVER
 var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
